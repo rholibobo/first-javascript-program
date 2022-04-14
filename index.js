4e81a4a8bc61e8ac98d931871b7d1e650e0b0211
rholibobo <rholibobo@gmail.com>
2022-04-14 12:13:04 +0100

    first JS program

---
+++ index.js
@@ -0,0 +1,27 @@
+// Program One
+const firstNumber = parseInt(prompt("Input first Number"));
+
+
+const secondNumber = parseInt(prompt("Input second Number"));
+
+
+const sum = firstNumber + secondNumber;
+
+alert(sum);
+
+
+
+// Program Two 
+let Name1 = prompt("Enter your First Name");
+firstName = Name1.slice(0,4);
+
+let Name2 = prompt("Enter your Last Name");
+lastName = Name2.slice(0,1);
+
+let user = firstName + lastName;
+
+let userId = user.toLowerCase();
+
+let output = "Your CapacityBay UserId is " + userId ;
+
+alert(output);
